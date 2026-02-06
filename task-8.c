ce5e4fe0e9a66db0706000240164c0f4aea2c4d0
Abeer654 <m.abeertauseef@gmail.com>
2026-02-06 21:40:42 +0500

    Add files via upload

---
+++ task-8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	int age;
+	float height;
+	char Grade;
+	
+	printf("Your Age: ");
+	scanf (" %d", &age);
+	
+	printf("Enter height:");
+	scanf(" %f", &height);
+	
+	printf("Enter Grade:");
+	scanf(" %c", &Grade);
+	
+	return 0;
+}
